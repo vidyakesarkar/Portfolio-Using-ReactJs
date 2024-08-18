20a66d96fceffbf3b4431ecc2240f9961375982e
vidyakesarkar <vidyakesarkar2501@gmail.com>
2024-08-18 15:02:29 +0530

    second commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/vidya-kesarkar-585267269">linkedin.com/vidyakesarkar</a>
+          <a href="https://www.linkedin.com/in/vidya-kesarkar-585267269">linkedin.com/in/vidya-kesarkar-585267269</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
